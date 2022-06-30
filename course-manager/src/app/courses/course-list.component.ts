e7144253e520ae6de89a402d31fd8a51d13d6617
Anderson <engenharia@hdeletro.com.br>
2022-06-30 14:24:29 -0300

    componentes com atributos interdependentes

--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -15,17 +15,17 @@ export class CourseListComponent implements OnInit{
         {
           id: 1,
           name: 'angular: forms',
-          imageUrl: '',
+          imageUrl: '/assets/images/forms.png',
           price: 10,
           code: 'XPS-8796',
           duration: 120,
-          rating: 5.4,
+          rating: 4.5,
           releaseDate: 'December, 2, 2019'
         },
         {
           id: 2,
           name: 'angular: HTTP',
-          imageUrl: '',
+          imageUrl: '/assets/images/http.png',
           price: 45.99,
           code: 'LKL-5421',
           duration: 80,
