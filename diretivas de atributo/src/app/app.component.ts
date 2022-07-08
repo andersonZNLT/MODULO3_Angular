bf8714f82171a3fb760c7fdd54b290c04ff6d025
andersonZNLT <anderson.znlt@gmail.com>
2022-07-07 22:11:55 -0300

    diretivas

--- diretivas de atributo/src/app/app.component.ts
+++ diretivas de atributo/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'diretivas_atributo';
+export class AppComponent implements OnInit{
+  title = 'Aula diretivas de atributo';
+  count = 0;
+
+  constructor(){
+
+  }
+  ngOnInit(): void {
+    let interval = setInterval(() => {
+      this.count++;
+      if(this.count === 10) {
+        clearInterval(interval);
+      }
+    }, 1000)
+  }
 }
