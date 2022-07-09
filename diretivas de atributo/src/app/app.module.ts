3ae24af8b45f87e99894d74aee5c44441fcf8fb8
andersonZNLT <anderson.znlt@gmail.com>
2022-07-08 21:10:39 -0300

    service people

--- diretivas de atributo/src/app/app.module.ts
+++ diretivas de atributo/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MarcaTextoDirective } from './shared/directives/marca-texto.directive';
+import { PeopleService } from './shared/services/people.service';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { MarcaTextoDirective } from './shared/directives/marca-texto.directive';
     FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [PeopleService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
